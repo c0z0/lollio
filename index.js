51a61ce154999945b87f3d7f9e41660235e000cc
Cosmin Serdean <cosmin.serdean@lola.tech>
2017-08-31 18:03:50 +0300

    load .env

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import next from 'next'
 import cookieParser from 'cookie-parser'
+require('dotenv').config()
 
 const port = parseInt(process.env.PORT, 10) || 3000
 const dev = process.env.NODE_ENV !== 'production'
@@ -8,26 +9,26 @@ const app = next({ dev })
 const handle = app.getRequestHandler()
 
 app.prepare().then(() => {
-	const server = express()
+  const server = express()
 
-	server.use(cookieParser())
+  server.use(cookieParser())
 
-	server.get('/', (req, res) => {
-		if (!req.cookies.session) return res.redirect('/login')
-		app.render(req, res, '/', req.query)
-	})
+  server.get('/', (req, res) => {
+    if (!req.cookies.session) return res.redirect('/login')
+    app.render(req, res, '/', req.query)
+  })
 
-	server.get('/login', (req, res) => {
-		if (req.cookies.session) return res.redirect('/')
-		app.render(req, res, '/login', req.query)
-	})
+  server.get('/login', (req, res) => {
+    if (req.cookies.session) return res.redirect('/')
+    app.render(req, res, '/login', req.query)
+  })
 
-	server.get('*', (req, res) => {
-		return handle(req, res)
-	})
+  server.get('*', (req, res) => {
+    return handle(req, res)
+  })
 
-	server.listen(port, err => {
-		if (err) throw err
-		console.log(`> Ready on http://localhost:${port}`)
-	})
+  server.listen(port, err => {
+    if (err) throw err
+    console.log(`> Ready on http://localhost:${port}`)
+  })
 })
