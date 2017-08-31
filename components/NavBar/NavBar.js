10420cbeace2b83bbf4a65ac9be2085d6767225f
Cosmin Serdean <cosmoserdean@gmail.com>
2017-08-31 14:54:15 +0300

    branding

--- components/NavBar/NavBar.js
+++ components/NavBar/NavBar.js
@@ -5,50 +5,61 @@ import Cookie from 'js-cookie'
 import Router from 'next/router'
 
 const Wrapper = styled.div`
-  display: flex;
-  justify-content: space-between;
-  border-bottom: 1px #ddd solid;
-  background: white;
-  align-items: center;
-  box-shadow: 2px 2px 30px -8px rgba(0, 0, 0, 0.2);
+	display: flex;
+	justify-content: space-between;
+	border-bottom: 1px #ddd solid;
+	background: white;
+	align-items: center;
+	box-shadow: 2px 2px 30px -8px rgba(0, 0, 0, 0.2);
 `
-const Title = styled.h3`
-  color: #ed174c;
-  padding-left: 24px;
-  padding-bottom: 0px;
+const Title = styled.h1`
+	color: #ed174c;
+	padding-left: 24px;
+	padding-bottom: 0px;
+	display: inline-block;
 `
 
-const Img = styled.img`height: 96px; opacity: 0;`
+const Img = styled.img`
+	height: 100px;
+	margin-left: -60px;
+	margin-right: -20px;
+	transition: all 0.2s ease-out;
+	&:hover {
+		margin-left: -5px;
+	}
+`
 
 const Menu = styled.div``
 
 const MenuItem = styled.a`
-  color: #ed174c !important;
-  margin: 16px !important;
-  cursor: pointer !important;
-  &:hover {
-    text-decoration: underline !important;
-  }
+	color: #ed174c !important;
+	margin: 16px !important;
+	cursor: pointer !important;
+	&:hover {
+		text-decoration: underline !important;
+	}
 `
 
 export default function NavBar({ activePage }) {
-  return (
-    <Wrapper>
-      <Img src="https://rtfm.lolacloud.com/download/attachments/3055373/lola-tech-standard.png?api=v2"/>
-      <Menu>
-        <Link href="/">
-          <MenuItem>My events</MenuItem>
-        </Link>
-        <MenuItem>Find events</MenuItem>
-        <MenuItem
-          onClick={() => {
-            Cookie.remove('session')
-            Router.replace('/login')
-          }}
-        >
-          Signout
-        </MenuItem>
-      </Menu>
-    </Wrapper>
-  )
+	return (
+		<Wrapper>
+			<Link to="/">
+				<Img src="/static/logo-horizontal.svg" />
+			</Link>
+			<Menu>
+				<Link href="/">
+					<MenuItem>My events</MenuItem>
+				</Link>
+				<MenuItem>Find events</MenuItem>
+				<MenuItem
+					onClick={() => {
+						Cookie.remove('session')
+						Router.replace('/login')
+					}}
+				>
+					Signout
+				</MenuItem>
+			</Menu>
+		</Wrapper>
+	)
 }
